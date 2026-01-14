d0a94196b30c2e35f4df133cbb852993d9b8c12e
Luís Magalhães <luismagalhaes3.lm@gmail.com>
2026-01-14 17:45:11 +0000

    Read recipients

--- API/api.py
+++ API/api.py
@@ -1,32 +1,26 @@
-import os
-
+# import flask module
+import pandas as pd
 from flask import Flask
 
+RECIPIENT_CSV_PATH = "../datasets/raw/recipient_waiting_list_raw.csv"
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# instance of flask application
+app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# home route that returns below text
+# when root url is accessed
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+@app.route("/recipients")
+def get_recipients():
+    df_recipients = pd.read_csv(RECIPIENT_CSV_PATH, sep=';', encoding="latin1")
+    return df_recipients.to_json(orient='records')
 
-    return app
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
+
+
+def read_df(df_path):
+    df = pd.read_csv(df_path, sep=';', encoding="latin1")
+    return df
